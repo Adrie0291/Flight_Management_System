7cfd1e044acd908e8f87e20c0e621df3779b17bd
Adriane0291 <adrianbloch01@gmail.com>
2022-09-12 21:57:07 +0200

    user_entity

--- src/main/java/com/sda/group2/Launcher.java
+++ src/main/java/com/sda/group2/Launcher.java
@@ -2,7 +2,7 @@ package com.sda.group2;
 
 public class Launcher {
 
-    public static void main(String[] args) {
+ public static void main(String[] args) {
         Account admin = new Admin(); // admin account created "na sztywno"
     }
 }
