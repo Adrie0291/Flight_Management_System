fe2dd997ecb9945d733234d83282f5adfc98122e
tchodzia <tchodzia@gmail.com>
2022-09-13 20:27:00 +0200

    Updated HQL Classes - Airport

--- src/main/java/com/sda/group2/hibernate/hql/Airport.java
+++ src/main/java/com/sda/group2/hibernate/hql/Airport.java
@@ -1,4 +1,19 @@
 package com.sda.group2.hibernate.hql;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "airport")
 public class Airport {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "airport_id")
+    private String airportId;
+    @Column
+    private String name;
+    @Column
+    private String country;
+
+
+
 }
