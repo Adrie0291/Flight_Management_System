32f9b98007be4837511ccf70dad7cbc6c80dbea1
tchodzia <tchodzia@gmail.com>
2022-09-15 17:35:57 +0200

    Flight, Airport classes; flights.txt, first exceptions

---
+++ src/main/java/com/sda/group2/exceptions/FlightMappingException.java
@@ -0,0 +1,15 @@
+package com.sda.group2.exceptions;
+
+public class FlightMappingException extends Exception {
+
+    private String line;
+
+    public FlightMappingException(String message, String line){
+        super(message);
+        this.line = line;
+    }
+
+    public String getLine() {
+        return line;
+    }
+}
--- src/main/java/com/sda/group2/hibernate/hql/Flight.java
+++ src/main/java/com/sda/group2/hibernate/hql/Flight.java
@@ -1,7 +1,11 @@
 package com.sda.group2.hibernate.hql;
 
+import com.sda.group2.FileInput;
+import com.sda.group2.exceptions.FileDBNotConnectionException;
+import com.sda.group2.exceptions.FlightMappingException;
+
 import javax.persistence.*;
-import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Entity
 @Table(name = "flight")
@@ -10,13 +14,44 @@ public class Flight {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "flight_id")
     private int flightId;
+    @Column(name = "flight_number")
+    private String flightNumber;
     @Column(name = "arrival_airport_id")
     private String arrivalAirportId;
     @Column(name = "departure_airport_id")
     private String departureAirportId;
     @Column(name = "arrival_datetime")
-    private LocalDateTime arrivalDateTime;
+    private LocalTime arrivalTime;
     @Column(name = "departure_datetime")
-    private LocalDateTime departureDateTime;
+    private LocalTime departureTime;
+
+    public Flight(String flightNumber, String arrivalAirportId, String departureAirportId, LocalTime arrivalDateTime, LocalTime departureDateTime) {
+        this.flightNumber = flightNumber;
+        this.arrivalAirportId = arrivalAirportId;
+        this.departureAirportId = departureAirportId;
+        this.arrivalTime = arrivalDateTime;
+        this.departureTime = departureDateTime;
+    }
+
+    public void loadFlightsFromFileIntoDb() {
+
+        FileInput input = new FileInput();
+        try {
+            // TODO tutaj input do bazy
+            System.out.println(input.readFlights());
+
+
+        } catch (FileDBNotConnectionException e) {
+            System.out.println("Wystąpił wyjątek:");
+            System.out.println(e.getMessage());
+
+        }catch (FlightMappingException e){
+            System.out.println("Wystąpił wyjątek:");
+            System.out.println(e.getMessage());
+            System.out.println(e.getLine());
+        }
+
+    }
+
 
 }
