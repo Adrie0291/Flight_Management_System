fa1e49a585b3ce136d25eb774cbc35cd1618f5fe
Lavcrd <zlatkov.panayotov@gmail.com>
2022-09-15 18:11:31 +0200

    menu mechanic

---
+++ src/main/java/com/sda/group2/interfaces/options/Logout.java
@@ -0,0 +1,17 @@
+package com.sda.group2.interfaces.options;
+
+import com.sda.group2.interfaces.UserOption;
+
+public class Logout implements UserOption {
+    @Override
+    public void invoke() {
+        System.out.println("Tymczasowy placeholder wzorcowy, w tym miejscu jest działanie po wybraniu opcji");
+    }
+
+    @Override
+    public String getMethodName() {
+        return "Logout"; //tutaj jest nazwa która wyświetla się w menu
+    }
+
+    //Ogólnie cała przestrzeń do rozwijania danej opcji
+}
