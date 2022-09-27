51c9236f8a7e2a723aed4e84eff6e1dbe268e6bb
Rabioxxx <szg.jasinski@gmail.com>
2022-09-27 16:26:39 +0200

    Replaced scanner with helper.

--- src/main/java/com/sda/group2/interfaces/options/FlightsFilteredPreview.java
+++ src/main/java/com/sda/group2/interfaces/options/FlightsFilteredPreview.java
@@ -1,34 +1,31 @@
 package com.sda.group2.interfaces.options;
 
 import com.sda.group2.DataBaseService;
+import com.sda.group2.Helper;
 import com.sda.group2.hibernate.hql.Flight;
 import com.sda.group2.hibernate.hql.users.Account;
 import com.sda.group2.interfaces.UserOption;
 
 import java.util.List;
-import java.util.Scanner;
 
 public class FlightsFilteredPreview implements UserOption {
     @Override
     public void invoke(Account account) {
+        //String filename = scanner.nextLine();
+
+        // title
+        System.out.println("Filter arrival flights by: ");
+        String arrival = Helper.getNextLine();
+
+        System.out.println("Filter departure flights by: ");
+        String departure = Helper.getNextLine();
         try {
-            Scanner scanner = new Scanner(System.in);
-            //String filename = scanner.nextLine();
-
-            // title
-            System.out.println("Filter arrival flights by: ");
-            String arrival = scanner.nextLine();
-
-            System.out.println("Filter departure flights by: ");
-            String departure = scanner.nextLine();
-
             DataBaseService dtb = new DataBaseService();
             List<Flight> flights = dtb.showFilteredFlights(arrival, departure);
 
             for (Flight flight : flights) {
                 System.out.println(flight);
             }
-
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/main/java/com/sda/group2/interfaces/options/FlightsPreview.java
+++ src/main/java/com/sda/group2/interfaces/options/FlightsPreview.java
@@ -1,18 +1,11 @@
 package com.sda.group2.interfaces.options;
 
-import com.sda.group2.AirportController;
 import com.sda.group2.DataBaseService;
-import com.sda.group2.FlightController;
-import com.sda.group2.PlaneController;
-import com.sda.group2.hibernate.hql.Airport;
 import com.sda.group2.hibernate.hql.Flight;
-import com.sda.group2.hibernate.hql.Plane;
 import com.sda.group2.hibernate.hql.users.Account;
 import com.sda.group2.interfaces.UserOption;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class FlightsPreview implements UserOption {
 
@@ -31,7 +24,6 @@ public class FlightsPreview implements UserOption {
             for (Flight flight : flights) {
                 System.out.println(flight);
             }
-
         } catch (Exception e) {
             e.printStackTrace();
         }
